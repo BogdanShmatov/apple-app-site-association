617f0cb8251d93c891efa222ab0cae046aa65cd0
Bogdan <shmatovb@mail.ru>
2021-12-05 14:07:45 +0600

    Initial Commit

---
+++ TestWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWallet
+//
+//  Created by Bogdan on 05.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
